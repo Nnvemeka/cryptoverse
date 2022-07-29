5176e1b43caa25b2cc909ac6452c045c6981a20f
nnvedward <nnvedward@gmail.com>
2022-07-29 15:58:38 +0100

    App component styling and navbar items setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
 import { Navbar } from './components'
+import './App.css'
 
 const App = () => {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined } from '@ant-design/icons'
-import logo from '../images/cryptocurrency.jpg'
+import logo from '../images/cryptocurrency.png'
 
 const Navbar = () => {
     return (
         <div className='nav-container'>
             <div className="logo-container">
-                <Avatar src={logo} size='large' />
+                <Avatar src={logo} size='large'/>
                 <Typography.Title level={2} className='logo'>
                     <Link to='/'>
                         Cryptoverse
@@ -18,6 +18,20 @@ const Navbar = () => {
 
             </Button> */}
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to='/news'>News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
