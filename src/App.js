16f5d54a4354d00124841c6bc760b66139152bfa
nnvedward <nnvedward@gmail.com>
2022-07-31 21:51:12 +0100

    Setup footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {  Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
 import { Navbar, Home, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
@@ -15,14 +15,25 @@ const App = () => {
         <Layout>
           <div className='routes'>
             <Routes>
-              <Route path='/' element={<Home />}/>
-              <Route path='/exchanges' element={<Exchanges />}/>
-              <Route path='/Cryptocurrencies' element={<Cryptocurrencies />}/>
-              <Route path='/crypto/:coinId' element={<CryptoDetails />}/>
-              <Route path='/news' element={<News />}/>
+              <Route path='/' element={<Home />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/Cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route path='/news' element={<News />} />
             </Routes>
           </div>
         </Layout>
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Crptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   )
