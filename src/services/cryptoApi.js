6949c6422b7ff2660582031f31fb6deafc093bb2
nnvedward <nnvedward@gmail.com>
2022-08-01 22:21:17 +0100

    Setup get cryptos query using redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '0e5b3c24f7mshebcd30c107e2872p1564a0jsnddfc087a3510',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'crptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const { useGetCryptosQuery } = cryptoApi
